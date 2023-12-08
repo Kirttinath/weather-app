53a793df6c06bec32c7d7f0bcccd9a7734ac432c
Kirttinath Ojha <kirttinathojha000@gmail.com>
2023-12-08 10:02:02 +0530

    creating div with search icon

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import './WeatherApp.css'
+import search_icon from '../Assets/search.png'
+import clear_icon from '../Assets/clear.png'
+import cloud_icon from '../Assets/cloud.png'
+import drizzle_icon from '../Assets/drizzle.png'
+import rain_icon from '../Assets/rain.png'
+import snow_icon from '../Assets/snow.png'
+import wind_icon from '../Assets/wind.png'
+import humidity_icon from '../Assets/humidity.png'
 
 
 const WeatherApp = () => {
   return (
-    <div>
-      
+    <div className='container'>
+      <div className="top-bar">
+        <input type="text" className='cityInput' placeholder='Search City'/>
+        <div className="serach-icon">
+            <img src={search_icon} alt="" />
+        </div>
+
+      </div>
     </div>
   )
 }
